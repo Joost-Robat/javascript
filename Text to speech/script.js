0f9c2938776d7a26727b204c1e143dc49a131057
Joost-Robat <30769@ma-web.nl>
2020-12-11 14:29:29 +0100

    Text to speech aangepast nu pakt hij altijd een random cijfer.

--- Text to speech/script.js
+++ Text to speech/script.js
@@ -4,12 +4,15 @@ let randImg = document.getElementById('randImg');
 let knop0 = document.getElementById('knop0');
 let knop1 = document.getElementById('knop1');
 
-const onderwerp = ["Wouter", "Joost", "Giel"];
-const werkwoord = ["likt", "is", "stinkt naar"];
-const restwoord = ["papier", "raar", "een pannekoek"];
+const onderwerp = ["Wouter", "Joost", "Giel", "Hidde"];
+const werkwoord = ["likt", "is", "stinkt naar", "was"];
+const restwoord = ["papier", "raar", "een pannekoek",  "een appel"];
 
 let plaatjes = ["https://cdn.discordapp.com/emojis/720648833079443506.png?v=1", "https://cdn.discordapp.com/emojis/735313106246565941.png?v=1", "https://cdn.discordapp.com/emojis/722206216360689674.png?v=1", "https://cdn.discordapp.com/emojis/747298464379174964.png?v=1"]
-let arrayLength = onderwerp.length;
+let arrayLengthOnd = onderwerp.length;
+let arrayLengthWW = werkwoord.length;
+let arrayLengthRW = restwoord.length;
+
 
 knop0.addEventListener('click', function(){
   main();
@@ -34,10 +37,12 @@ function randomizer(range = 1){
 }
 
 function sentence(){
-  let index = randomizer(arrayLength);
-  let woord1 = onderwerp[index];
-  let woord2 = werkwoord[index];
-  let woord3 = restwoord[index];
+  let index0 = randomizer(arrayLengthOnd);
+  let woord1 = onderwerp[index0];
+  let index1 = randomizer(arrayLengthWW);
+  let woord2 = werkwoord[index1];
+  let index2 = randomizer(arrayLengthRW);
+  let woord3 = restwoord[index2];
   let outputString = woord1 + " " + woord2 + " " + woord3;
   return outputString;
 }
@@ -52,6 +57,6 @@ function sayWords (textString) {
 }
 
 function selectImg(){
-  let index = randomizer(arrayLength);
+  let index = randomizer(plaatjes.length);
   randImg.src = plaatjes[index];
 }
